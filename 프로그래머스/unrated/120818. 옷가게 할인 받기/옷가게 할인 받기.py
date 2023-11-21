65774ccd0604e43b217aa78285bc31e3b6e49bf1
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2023-11-21 20:08:54 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,11 @@
+def solution(price):
+    if price>=500000:
+        price= int(price*0.8)
+    elif price>=300000:
+        price=int(price*0.9)
+    elif price>=100000:
+        price=int(price*0.95)
+    else:
+        return price
+    answer = price
+    return answer
