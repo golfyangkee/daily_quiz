d4a225c700fddde8a202326fc2d5ccf1254fcb93
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2023-11-29 00:41:29 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.py
@@ -12,4 +12,8 @@ def solution(num, k):
     #     if str(k) == n:
     #         return i + 1
     #     return -1
+    
+    # 다른 풀이3
+    # return -1 if str(k) not in str(num) else str(num).find(str(k)) + 1
+    
     return answer
