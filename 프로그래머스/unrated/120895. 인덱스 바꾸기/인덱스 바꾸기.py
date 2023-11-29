f1908013be128e9245e09fd31834f11cfea89d5a
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2023-11-30 00:12:00 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -9,4 +9,10 @@ def solution(my_string, num1, num2):
     my_list.pop(num1+1)
     for i in my_list:
         answer += i
+        
+    # 다른 풀이
+    # def solution(my_string, num1, num2):
+    # s = list(my_string)
+    # s[num1],s[num2] = s[num2],s[num1]
+    # return ''.join(s)
     return answer
