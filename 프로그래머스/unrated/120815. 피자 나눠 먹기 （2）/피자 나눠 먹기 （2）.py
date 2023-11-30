19852084193b1a340079309cf4f14eb85c9a6ae8
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2023-11-30 11:42:52 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    # num 피자 개수
+    # 6*num=n의 배수인 num이 가장 작은 수
+    # num은 6*n을 넘을 수 없다.
+    for num in range(1,6*n+1):
+        if (6*num)%n==0:
+            answer=num
+            break
+    return answer
