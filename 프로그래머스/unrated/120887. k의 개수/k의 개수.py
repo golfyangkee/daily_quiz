90055c5cbd8db10a74219687219bd5cf645f3cf3
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2023-11-30 11:59:12 +0900

    [level unrated] Title: k의 개수, Time: 11.38 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120887. k의 개수/k의 개수.py
+++ 프로그래머스/unrated/120887. k의 개수/k의 개수.py
@@ -6,4 +6,10 @@ def solution(i, j, k):
         if str_k in str_num:
             print(str_num)
             answer += 1*str_num.count(str_k)
+            
+    # 다른풀이1
+    # return sum(map(lambda v: str(v).count(str(k)), range(i, j+1)))
+    
+    # 다른풀이2
+    # answer = sum([ str(i).count(str(k)) for i in range(i,j+1)])
     return answer
