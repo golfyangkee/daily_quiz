adf733759238cf191fe74eb91b0bb701c8dda3a4
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2023-12-05 00:23:12 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -0,0 +1,9 @@
+def solution(num_list, n):
+    res = []
+    for i in range(0, len(num_list), n):
+        temp = []
+        for j in range(0, n):
+            temp.append(num_list[i])
+            i = i + 1
+        res.append(temp)
+    return res
