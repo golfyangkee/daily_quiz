7df6721a2e5719064181467ee86be17ff7f59f49
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2023-12-04 23:46:32 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: ., Memory: undefined -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,7 +1,6 @@
 def solution(numbers):
-    answer = 0
     numbers.sort()
-    max_a=numbers[0]*numbers[1]
-    max_b=numbers[-1]*numbers[-2]
-    answer=max(max_a,max_b)
+    num1=numbers[0]*numbers[-1]
+    num2=numbers[-1]*numbers[-2]
+    answer=max(num1,num2)
     return answer
