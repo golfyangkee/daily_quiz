edbc0c068472c2d97e0d0eb08236c08b66304933
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2024-01-02 23:38:18 +0900

    [level unrated] Title: 할 일 목록, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
@@ -4,3 +4,6 @@ def solution(todo_list, finished):
         if int(finished[i])==0:
             answer.append(todo_list[i])
     return answer
+
+# 다른 풀이
+# return [work for idx, work in enumerate(todo_list) if not finished[idx]]
