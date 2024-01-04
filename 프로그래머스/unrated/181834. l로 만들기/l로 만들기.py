ba1fd3dbed34561caa8d9b2c507fd4091489e6f6
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2024-01-04 13:31:51 +0900

    [level unrated] Title: l로 만들기, Time: 0.63 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
@@ -2,3 +2,7 @@ def solution(myString):
     answer = ''
     answer=myString.replace('a','l').replace('b','l').replace('c','l').replace('d','l').replace('e','l').replace('f','l').replace('g','l').replace('h','l').replace('i','l').replace('j','l').replace('k','l')  
     return answer
+
+# 다른 풀이
+#    answer = [x if x > 'l' else 'l' for x in myString]
+#    return ''.join(answer)
