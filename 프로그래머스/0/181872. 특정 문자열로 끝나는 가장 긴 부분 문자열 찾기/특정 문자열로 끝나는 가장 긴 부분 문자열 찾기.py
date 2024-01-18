a6cdddd785942d2fb8c16f794c986a8bf5ecf6f9
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2024-01-18 16:41:39 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -0,0 +1,9 @@
+def solution(myString, pat):
+    answer = ''
+    re_s=myString[::-1]
+    re_p=pat[::-1]
+    if re_p in re_s:
+        a=re_s.index(re_p)
+        re_a=re_s[a:]
+        answer=re_a[::-1]
+    return answer
