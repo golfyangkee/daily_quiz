fd995b24102bcc8c53acd6ac527e8ce56d2d38f3
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2024-01-24 19:36:43 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -1,12 +1,16 @@
 def solution(myString, pat):
-    start = 0
-    cnt = 0
-    
-    while True:
-        idx = myString.find(pat, start)
-        if idx == -1:
-            break
-        cnt += 1
-        start = idx + 1
-    
-    return cnt
+    # start = 0
+    # cnt = 0
+    # while True:
+    #     idx = myString.find(pat, start)
+    #     if idx == -1:
+    #         break
+    #     cnt += 1
+    #     start = idx + 1
+    # return cnt
+
+    answer = 0
+    for i, x in enumerate(myString) :
+        if myString[i:].startswith(pat) :
+            answer += 1
+    return answer
