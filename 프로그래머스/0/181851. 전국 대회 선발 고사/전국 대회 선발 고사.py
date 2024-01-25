cc26ef3f5301f5d36b228c5613dc21f238ba6cf2
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2024-01-25 09:38:12 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -0,0 +1,12 @@
+def solution(rank, attendance):
+    n = len(rank)
+    answer =0
+    r_a = []
+
+    for i in range(n):
+        if attendance[i]:
+            r_a.append([rank[i], i])
+#    print(r_a) 	[[7, 1], [2, 2], [5, 3], [4, 4]]
+    r_a.sort(key = lambda v : v[0])
+# r_a를 정렬하는데, 이때 정렬 기준으로 각 요소의 첫 번째 원소(v[0])를 사용한다는 것을 의미
+    return 10000 * r_a[0][1] + 100 * r_a[1][1] + r_a[2][1]
