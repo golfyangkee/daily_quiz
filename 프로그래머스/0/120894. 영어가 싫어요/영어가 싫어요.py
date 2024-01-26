e56f258c74f82389745b2b0dd678e93cb094178f
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2024-01-26 11:14:45 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,5 @@
+def solution(numbers):
+    nums=["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for index, num in enumerate(nums):
+        numbers=numbers.replace(num,str(index))
+    return int(numbers)
