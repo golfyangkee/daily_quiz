0a69c999507bac12ef4a84657032cb1a5fd8088b
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2024-01-30 21:36:27 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -0,0 +1,10 @@
+def solution(score):
+    answer = [] # 등수
+    avg=[sum(i)/2 for i in score] # 평균
+    avg_sorted=sorted(avg, reverse=True) # 평균을 큰 값부터 내림차순 정렬
+
+    answer =[] # 등수
+    for i in avg:
+        answer.append(avg_sorted.index(i)+1) # index는 맨 앞에 위치한 요소의 인덱스 번호를 리턴
+        
+    return answer
