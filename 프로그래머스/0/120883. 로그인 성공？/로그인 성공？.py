17c8e65b781bda291b268e3c70ecc470aa433a3f
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2024-02-04 10:21:41 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -10,3 +10,8 @@ def solution(id_pw, db):
             if pw!=id_pw[1]:
                 answer='fail'
     return answer
+    # if db_pw := dict(db).get(id_pw[0]):
+    #     return "login" if db_pw == id_pw[1] else "wrong pw"
+    # return "fail"
+#  Python 3.8부터 도입된 "walrus 연산자(walrus operator)"
+# 대입문과 표현식을 결합하여 한 번의 평가에서 값을 변수에 할당하고 그 값을 다시 사용할 수 있게 합니다.
