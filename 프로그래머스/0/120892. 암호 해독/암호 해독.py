b49d610d7856bcd3bae11765bf87c122cdcecc77
golfyangkee <103293064+golfyangkee@users.noreply.github.com>
2024-05-14 12:37:26 +0900

    [level 0] Title: 암호 해독, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -3,3 +3,7 @@ def solution(cipher, code):
     for i in range(1, len(cipher)//code+1):
         answer=answer+cipher[(code*i)-1]
     return answer
+'''
+슬라이스 이용하기
+    return cipher[code-1::code]
+'''
