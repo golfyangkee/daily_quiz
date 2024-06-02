3778749fb442cc924359b5d9935555ccaaac51d4
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-02 10:01:27 +0900

    Update FrontEnd 개발자 찾기.sql

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -16,6 +16,8 @@ FROM DEVELOPERS D JOIN FE ON D.SKILL_CODE & FE.CODE = FE.CODE
 )
 
 -- 정답
+-- JOIN을 사용하면 여러개의 스킬을 보유한 경우 중복발생
+-- DISTINCT ID 를 여기서 하던 아니면 위에서 해야한다.
 SELECT DISTINCT ID, EMAIL, FIRST_NAME, LAST_NAME
 FROM COR
 ORDER BY ID;
