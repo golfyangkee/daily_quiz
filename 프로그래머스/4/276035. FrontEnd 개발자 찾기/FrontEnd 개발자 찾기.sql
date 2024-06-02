cf0c3290ab699f01d695a35081aadc72ef8fa525
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-02 09:59:19 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,21 @@
+-- front end 코드 알기
+-- CATEGORY 가 FRONT END
+-- javascript, react, vue
+WITH FE AS (
+SELECT *
+FROM SKILLCODES 
+WHERE CATEGORY = 'Front End'
+),
+-- 스킬코드가 맞는지
+-- FE의 CODE랑 개발자가 SKILL_CODE랑 &했을 때 CODE랑 같으면
+-- CODE들 중 하나라도 가지고 있으면 
+-- CODE = CODE & SKILL_CODE
+COR AS (
+SELECT *
+FROM DEVELOPERS D JOIN FE ON D.SKILL_CODE & FE.CODE = FE.CODE
+)
+
+-- 정답
+SELECT DISTINCT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM COR
+ORDER BY ID;
