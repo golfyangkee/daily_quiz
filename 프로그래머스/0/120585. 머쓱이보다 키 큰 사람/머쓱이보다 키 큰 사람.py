e05fd4d9bd01c3f176d6314f235be8ce11608718
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-03 21:30:33 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,6 +1,14 @@
 def solution(array, height):
-    answer = 0
+    # for 문 사용
+    answer=0
     for i in array:
-        if i > height:
+        if height < i:
             answer+=1
     return answer
+
+    # 슬레이싱으로 찾아보기-> 같은 키이면 적용 못함
+    # array.append(height)
+    # array.sort()
+    # num = array.index(height)
+    # array = array[num+1:]
+    # return len(array)
