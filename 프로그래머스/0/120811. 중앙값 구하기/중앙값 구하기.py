5cd85f565b3485f74da1a4d6a84352354039f3d7
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-03 21:52:03 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,6 +1,4 @@
 def solution(array):
-    answer = 0
-    array.sort()
-    num=len(array)//2
-    answer=array[num]
-    return answer
+    array.sort() # 정렬
+    num = len(array)//2 # 나누기 2의 몫 = 인덱스
+    return array[num]
