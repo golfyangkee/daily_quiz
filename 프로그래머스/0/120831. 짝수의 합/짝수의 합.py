825e039e16fbf0e948c164bba2c2158dca25b995
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-04 09:13:55 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -1,6 +1,10 @@
 def solution(n):
-    answer = 0
+    answer1 = 0
     for i in range(1,n+1):
         if i%2==0:
-            answer +=i
-    return answer
+            answer1 +=i
+   
+    # sum 함수로 풀기
+    return sum([i for i in range(2,n+1, 2)])
+
+    
