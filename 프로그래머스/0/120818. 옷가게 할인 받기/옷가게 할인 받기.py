6ff589eb3d010f9f6f2667ca88681061d92cc6b4
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-06 07:53:38 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,12 +1,9 @@
 def solution(price):
-    answer=0
-    # 소수점 이하를 버린 정수를 return
-    if price >= 500000:
-        answer= int(price*0.8)
-    elif price >= 300000:
-        answer = int(price*0.9)
-    elif price >= 100000:
-        answer = int(price*0.95)
+    if price >=500000:
+        return int(price*0.8)
+    elif price>=300000:
+        return int(price*0.9)
+    elif price>=100000:
+        return int(price*0.95)
     else:
-        answer=price
-    return answer
+        return price
