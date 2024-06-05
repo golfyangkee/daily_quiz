8368798b9c005ce36c793bd5cde4dfe13149de76
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-05 13:06:54 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,10 +1,8 @@
 def solution(num_list):
-    odd=0
-    even=0
+    even = 0
+    odd = 0
     for i in num_list:
         if i%2==0:
             even+=1
-        elif i%2==1:
-            odd+=1
-    answer = [even, odd]
-    return answer
+        else: odd+=1 
+    return [even, odd]
