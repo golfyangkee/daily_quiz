953405a7f2a11f205dac3d4a4d46e8b1285942a3
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-07 06:52:09 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,14 +1,19 @@
 def solution(array):
-    answer = 0
-    my_list=list(set(array))
-    cnt=[]
-    for i in my_list:
-        cnt.append(array.count(i))
-    a=max(cnt)
-    b=cnt.index(a)
-    cnt.remove(a)
-    print(cnt)
-    if a in cnt:
-        return -1
+    test=list(set(array)) # 중복 숫자 제거
+    answer={} # 요소 개수 저장할 튜플
+
+    # 각각의 요소 개수 저장
+    values=[]
+    answer = {num:array.count(num) for num in test}
+    for i in answer.values():
+        values.append(i)
+    
+    # 최빈값의 개수
+    max_num=max(values)
+    
+    # 최빈값의 값을 알기 위해
+    if values.count(max_num) == 1:
+        fre= [i for i, values in answer.items() if values==max_num]
+        return int(''.join(map(str, fre)))
     else:
-        return my_list[b]
+        return -1
