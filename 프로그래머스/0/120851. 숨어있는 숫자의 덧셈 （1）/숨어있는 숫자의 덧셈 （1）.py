b9f0f100cf1315607623843542385f411463886d
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-09 13:01:53 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,13 +1,18 @@
 def solution(my_string):
+    # 예외구문 사용하기
+    # answer = 0
+    # for i in my_string:
+    #     try:
+    #         # 숫자가 되면 더하기
+    #         answer += int(i)
+    #     except ValueError:
+    #         # 밸류에러 나면 pass
+    #         pass
+    # return answer
+
+    # isdigit() 함수 이용하기 - 숫자인지 판별
     answer = 0
     for i in my_string:
-        if i == '1' or i == '2' or i == '3' or i == '4' or i == '5' or i == '6'or i == '7' or i == '8' or i == '9':
-            answer=answer+int(i)
+        if i.isdigit():
+            answer+=int(i)
     return answer
-
-'''
-isdigit()
-isnumeric()
-함수 사용하기
-    return sum(int(i) for i in my_string if i.isdigit())
-'''
