36742e5960941ad4432d9cdeec6fa4e575a5248b
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-10 20:02:29 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,3 +1,6 @@
 def solution(my_string):
-    answer = my_string.replace('a','').replace('e','').replace('i','').replace('o','').replace('u','')
-    return answer
+    # answer = my_string.replace('a','').replace('e','').replace('i','').replace('o','').replace('u','')
+    # return answer
+
+     # 다른 방식으로 풀어보기
+    return ''.join(i for i in my_string if not (i in 'aeiou'))
