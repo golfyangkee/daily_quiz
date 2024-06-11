6ff9d25daaef0af28e59d941b805d4d390e087f8
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-12 06:45:50 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,15 +1,6 @@
 def solution(rsp):
-    answer =''
+    answer = ''
+    res = {'2':'0', '0':'5', '5':'2'}
     for i in rsp:
-        if i == '2':
-            answer += '0'
-        elif i =='0':
-            answer += '5'
-        elif i == '5':
-            answer += '2'
+        answer+=res[i]
     return answer
-'''
-# 튜플로 풀어보기
-    d = {'0':'5','2':'0','5':'2'}
-    return ''.join(d[i] for i in rsp)
-'''
