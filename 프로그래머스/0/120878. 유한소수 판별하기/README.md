0c776371edd40af200acc1a32955f5f0b3b8f558
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-17 10:50:15 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/README.md
+++ 프로그래머스/0/120878. 유한소수 판별하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 유한소수 판별하기 - 120878 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120878) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120878#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 10.1 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 20:59:41
+2024년 06월 17일 10:50:12
 
 ### 문제 설명
 
--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,10 +1,30 @@
-from math import gcd
-
 def solution(a, b):
-    b //= gcd(a,b)
-    while b%2==0:
-        b//=2
-    while b%5==0:
-        b//=5
-    return 1 if b==1 else 2
-# gcb= 최대 공약수 구하는 함수
+    answer = 2
+    b_list = []
+    # a 랑 b 공통 인수로 나누기
+    for i in range(2, 1001):
+        if a%i==0 and b%i ==0:
+            a //= i
+            b //= i
+            
+    # b 약수 찾기
+    # b_copy = b
+    # for num in range(2, b_copy+1):
+    #     if b%num == 0:
+    #         b_list.append(num)
+    #         b //= num
+    div =2
+    while b > 1 :
+        if b%div == 0:
+            b_list.append(div)
+            b = b//div
+            # 계속 나눔
+        else: 
+            # 2로 안 나눠지면 하나씩 키움
+            div+=1
+
+
+    if all(x in [2, 5] for x in b_list):
+    # if b_list == [5] or b_list==[2] or b_list ==[2,5]:
+        answer = 1
+    return answer
