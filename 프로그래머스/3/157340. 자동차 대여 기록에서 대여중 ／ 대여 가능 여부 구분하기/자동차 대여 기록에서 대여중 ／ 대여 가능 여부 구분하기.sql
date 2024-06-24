ccfb405f1dd6b8244e3171e73f3dacc20aa781f9
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-24 17:28:40 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,16 +1,20 @@
--- 코드를 입력하세요
-SELECT CAR_ID, CASE WHEN SUM( 
-                CASE WHEN DATEDIFF('2022-10-16', START_DATE) >=0 AND DATEDIFF('2022-10-16', END_DATE)<=0 THEN 1 ELSE 0 END) = 0
-                THEN '대여 가능' ELSE '대여중' END AS AVAILABILITY
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
-GROUP BY CAR_ID
-ORDER BY CAR_ID DESC;
+-- 16일 대여
+-- START_DATE< 16일 AND END_DATE >=16일 이면 대여 중
+-- END_DATE가 16일 전 이거나, START_DATE가 16일 이후면 대여 가능
+-- 
+SELECT CAR_ID, CASE WHEN SUM(CASE WHEN '2022-10-16' BETWEEN START_DATE AND END_DATE THEN 1
+ELSE 0 END) = 1 THEN '대여중' ELSE '대여 가능' 
+END AS AVAILABILITY 
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY 1
+ORDER BY 1 DESC;
 
-# SELECT CAR_ID, CASE WHEN SUM(
-#     CASE WHEN START_DATE <= '2022-10-16' 
-#     AND END_DATE >= '2022-10-16' 
-#     THEN 1 ELSE 0 END) = 0 
-#     THEN '대여 가능' ELSE '대여중' END AS AVAILABILITY
-# FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-# GROUP BY CAR_ID
-# ORDER BY CAR_ID DESC;
+# SELECT car_id,
+# case when 
+# sum(case when '2022-10-16' between start_date and end_date then 1
+# else 0 end) =1 then '대여중'
+# else '대여 가능'
+# end availability
+# from car_rental_company_rental_history
+# group by 1
+# order by 1 desc;
