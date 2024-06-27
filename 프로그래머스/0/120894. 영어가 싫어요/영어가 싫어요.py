d5fb2d5e36962aff7b8f0f7d4675f5a061199fd5
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-27 19:56:35 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,5 +1,7 @@
 def solution(numbers):
-    nums=["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    for index, num in enumerate(nums):
-        numbers=numbers.replace(num,str(index))
-    return int(numbers)
+    answer = 0
+    # dict1 = {'one': 1, 'two': 2, 'three': 3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    # answer = numbers.replace(dict1)
+    # if 'one' in numbers:
+    answer = numbers.replace('one', '1').replace('two', '2').replace('three', '3').replace('four', '4').replace('five', '5').replace('six', '6').replace('seven', '7').replace('eight', '8').replace('nine', '9').replace('zero', '0')
+    return int(answer)
