c4372e893c677dae5a7afc480375f6e83e7f7b73
elly <103293064+golfyangkee@users.noreply.github.com>
2024-06-27 20:07:16 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,9 +1,3 @@
 def solution(cipher, code):
-    answer = ''
-    for i in range(1, len(cipher)//code+1):
-        answer=answer+cipher[(code*i)-1]
+    answer = cipher[code-1::code]
     return answer
-'''
-슬라이스 이용하기
-    return cipher[code-1::code]
-'''
