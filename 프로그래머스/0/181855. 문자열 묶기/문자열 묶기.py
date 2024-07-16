e57ec3c8e970a4bae92db71d6dbe5899316ac5be
elly <103293064+golfyangkee@users.noreply.github.com>
2024-07-16 11:14:12 +0900

    [level 0] Title: 문자열 묶기, Time: 24.44 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,6 +1,17 @@
 def solution(strArr):
-    my_list = [len(i) for i in strArr]
-    answer = []
-    for i in set(my_list):
-        answer.append(my_list.count(i))
-    return max(answer)
+    answer = 0
+    result=[]
+    for i in strArr: # 각 원소들의 길이를 result 리스트에 넣음
+        result.append(len(i))
+    
+    c_r = {} # 딕트 써서 key value로 만듦
+    for i in result:
+        if i in c_r:
+            c_r[i]+=1
+        else:
+            c_r[i]=1
+    
+    list1 = []
+    list1 = list(c_r.values())
+    list1.sort()
+    return list1[-1]
