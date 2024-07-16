1c374faa941332a0b1f55e9aa755ab5ed81dc608
elly <103293064+golfyangkee@users.noreply.github.com>
2024-07-16 10:55:25 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,13 +1,9 @@
 def solution(rank, attendance):
-    n = len(rank)
-    answer =0
-    r_a = []
-
-    for i in range(n):
-        if attendance[i]:
-            r_a.append([rank[i], i])
-#    print(r_a) 	[[7, 1], [2, 2], [5, 3], [4, 4]]
-    r_a.sort(key = lambda v : v[0])
-# r_a를 정렬하는데, 이때 정렬 기준으로 각 요소의 첫 번째 원소(v[0])를 사용한다는 것을 의미
-    return 10000 * r_a[0][1] + 100 * r_a[1][1] + r_a[2][1]
-# arr = sorted([(x, i) for i, x in enumerate(rank) if attendance[i]])
+    answer = 0
+    result = []
+    for i in range(len(attendance)):
+        if attendance[i] == True:
+            result.append(rank[i])
+    result.sort()
+    answer = rank.index(result[0])*10000 + rank.index(result[1])*100 + rank.index(result[2])
+    return answer
