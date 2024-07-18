e7a002f7c2144e0acbdb770bf483ad3b2a8de5e8
elly <103293064+golfyangkee@users.noreply.github.com>
2024-07-18 11:40:25 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -0,0 +1,8 @@
+def solution(myString, pat):
+    myString = myString.replace('A','C')
+    myString = myString.replace('B', 'A')
+    myString = myString.replace('C','B')
+    if pat in myString:
+        return 1
+    else:
+        return 0
