215b66c10517f5ca4de7c8a533a9ddeac384b962
elly <103293064+golfyangkee@users.noreply.github.com>
2024-07-22 12:40:03 +0900

    [level 0] Title: 세로 읽기, Time: 0.49 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -0,0 +1,9 @@
+def solution(my_string, m, c):
+    answer = ''
+    result = []
+    for i in range(len(my_string)//m): # 짜른 문자 result 리스트에 저장
+        result.append(my_string[m*i:m*i+m])
+        
+    for r in result: # 리스트 원소들 하나씩 빼서 c번째 answer에 넣기
+        answer += r[c-1] # 배열로 보면 -1 해줘야 해당 번째니까
+    return answer
