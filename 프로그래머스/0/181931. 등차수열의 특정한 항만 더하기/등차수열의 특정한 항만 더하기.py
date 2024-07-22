c38fcd7072868601ef9ede6761b01ef2dcedd15a
elly <103293064+golfyangkee@users.noreply.github.com>
2024-07-22 12:33:15 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -0,0 +1,16 @@
+'''
+등차수열이란?
+등차수열(等差數列)은 각 항이 일정한 차이를 가지며 나열된 수열을 말합니다. 이 일정한 차이를 공차(公差)라고 합니다. 예를 들어, 수열 2, 5, 8, 11, 14는 공차가 3인 등차수열입니다. 각 항을 이전 항에 공차를 더한 값으로 얻을 수 있습니다.
+'''
+'''
+정리하면 True만 더해
+'''
+def solution(a, d, included):
+    answer = 0
+    
+    # true만 더해보자.
+    for i in range(len(included)):
+        if included[i] ==True:
+            answer += a + d*i
+
+    return answer
