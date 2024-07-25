ae224d09ab54741776a07288b70f63472c0f9a4e
elly <103293064+golfyangkee@users.noreply.github.com>
2024-07-25 10:53:42 +0900

    [level 0] Title: qr code, Time: 0.24 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.py
+++ 프로그래머스/0/181903. qr code/qr code.py
@@ -1,6 +1,6 @@
 def solution(q, r, code):
     answer = ''
-    for i in range(0,len(code)):
-        if i%q==r:
-            answer+=code[i]
+    for i in range(len(code)):
+        if i%q == r:
+            answer += code[i]
     return answer
