4e6a03d7f95b4190b7737e248bf51c626e6ee758
elly <103293064+golfyangkee@users.noreply.github.com>
2024-07-25 10:46:14 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,15 +1,9 @@
--- 파이썬 씨샵 스킬코드 출력
-# SELECT CODE
-# FROM SKILLCODES 
-# WHERE NAME IN ('Python', 'C#');
-
--- 정답
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
 FROM DEVELOPERS
-WHERE SKILL_CODE & (SELECT CODE
+WHERE SKILL_CODE & ((SELECT CODE
                     FROM SKILLCODES 
-                WHERE NAME = 'Python')
-    OR SKILL_CODE & (SELECT CODE
+                    WHERE NAME = 'C#') + 
+                    (SELECT CODE
                     FROM SKILLCODES 
-                WHERE NAME ='C#')
-ORDER BY ID;
+                    WHERE NAME = 'Python'))!=0
+ORDER BY 1;
