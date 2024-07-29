bdd211337e568553495cbf529bca32637dd9fb86
elly <103293064+golfyangkee@users.noreply.github.com>
2024-07-29 13:45:17 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.77 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,7 +1,11 @@
 def solution(arr, intervals):
-    answer = []
-    for i in intervals:
-        a, b = i[0], i[1]
-        for i in range(a,b+1):
-            answer.append(arr[i])
-    return answer
+    # answer = []
+    # for i in intervals:
+    #     a, b = i[0], i[1]
+    #     for i in range(a,b+1):
+    #         answer.append(arr[i])
+    # return answer
+
+    a1, b1 = intervals[0]
+    a2, b2 = intervals[1]
+    return arr[a1:b1+1] + arr[a2:b2+1]
