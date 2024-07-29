0a261886a34a6ac78ef84b75c1fd9e8c54b19b7a
elly <103293064+golfyangkee@users.noreply.github.com>
2024-07-29 13:32:36 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,9 +1,13 @@
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
-FROM DEVELOPERS
-WHERE SKILL_CODE & ((SELECT CODE
-                    FROM SKILLCODES 
-                    WHERE NAME = 'C#') + 
-                    (SELECT CODE
-                    FROM SKILLCODES 
-                    WHERE NAME = 'Python'))!=0
+FROM DEVELOPERS 
+WHERE (SELECT CODE
+        FROM SKILLCODES 
+        WHERE NAME = 'Python') = SKILL_CODE & (SELECT CODE
+                                                FROM SKILLCODES 
+                                                 WHERE NAME = 'Python')
+OR (SELECT CODE
+    FROM SKILLCODES 
+    WHERE NAME = 'C#') = SKILL_CODE & (SELECT CODE
+                                        FROM SKILLCODES 
+                                        WHERE NAME = 'C#')
 ORDER BY 1;
