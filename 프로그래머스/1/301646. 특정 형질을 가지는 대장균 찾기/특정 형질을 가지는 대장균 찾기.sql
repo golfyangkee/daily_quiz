3ee776b4b45b19fc36eee9c91aadbde2617ea345
elly <103293064+golfyangkee@users.noreply.github.com>
2024-07-31 11:18:26 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,6 +1,6 @@
 SELECT COUNT(DISTINCT ID) AS COUNT
 FROM ECOLI_DATA 
-WHERE (GENOTYPE&5) AND !(GENOTYPE&2);
+WHERE ((GENOTYPE&1) OR (GENOTYPE&4)) AND !(GENOTYPE&2);
 
 # 문제에서 형질의 번호는 2진수에서 1의 위치, 값은 별개로 2진수 자체값으로 비교를 하기 때문입니다.
 # genotype이 1111(2)일 경우
