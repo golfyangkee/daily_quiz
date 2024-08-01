ea21c8cb2dcf51597be5b19e713fb9bd35462a45
elly <103293064+golfyangkee@users.noreply.github.com>
2024-08-01 14:43:27 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 13.40 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -1,9 +1,6 @@
 def solution(arr, queries):
-    answer = []
     for query in queries:
-        s = query[0]
-        e = query[1]
-        for i in range(len(arr)):
-            if s <= i <= e:
-                arr[i]=arr[i]+1
+        s, e = query[0], query[1]
+        for i in range(s, e+1):
+            arr[i] = arr[i]+1
     return arr
