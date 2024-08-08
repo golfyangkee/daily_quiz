e02516b4ae1584821db341d976ac6d6ab201de2a
elly <103293064+golfyangkee@users.noreply.github.com>
2024-08-08 20:04:05 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,6 +1,2 @@
 def solution(my_string, s, e):
-    s1 = my_string[:s]
-    s2 = my_string[s:e+1]
-    s3 = my_string[e+1:]
-    answer = s1 + s2[::-1] + s3
-    return answer
+    return my_string[:s]+''.join(reversed(my_string[s:e+1]))+my_string[e+1:]
