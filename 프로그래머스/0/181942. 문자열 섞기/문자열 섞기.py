4917da96d57049c5888cc265df8c8389ad30bff6
elly <103293064+golfyangkee@users.noreply.github.com>
2024-08-16 20:54:12 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,6 +1,5 @@
 def solution(str1, str2):
     answer = ''
-    num = len(str1)
-    for i in range(num):
-        answer+= str1[i] + str2[i]
+    for i in range(len(str1)):
+        answer += str1[i]+str2[i]
     return answer
