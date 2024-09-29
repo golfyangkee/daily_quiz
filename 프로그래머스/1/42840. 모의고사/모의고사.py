9a13b6b88f0bae4a1d05178f9de1d23164e45315
elly <103293064+golfyangkee@users.noreply.github.com>
2024-09-29 02:34:58 -0400

    [level 1] Title: 모의고사, Time: 6.90 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,23 +1,26 @@
 def solution(answers):
-    # 수포자들의 답 패턴
-    pattern1 = [1, 2, 3, 4, 5]
-    pattern2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    answer = [] # 리턴할 값들
     
-    # 수포자별로 맞춘 문제 수를 기록할 리스트
-    scores = [0, 0, 0]
+    # 1~3번 수포자들의 정답 패턴 입력
+    a1 = [1,2,3,4,5]
+    a2 = [2,1,2,3,2,4,2,5]
+    a3 = [3,3,1,1,2,2,4,4,5,5]
     
-    # 정답과 패턴을 비교하여 맞춘 문제 수 계산
+    # 1~3번 수포자들의 맞춘 개수 입력
+    score = [0,0,0]
+    
+    # answers의 답과 비교해서 +1 시킴, elif 가 아니라 꼭 if 로 해야 한다.
     for i in range(len(answers)):
-        if answers[i] == pattern1[i % len(pattern1)]:
-            scores[0] += 1
-        if answers[i] == pattern2[i % len(pattern2)]:
-            scores[1] += 1
-        if answers[i] == pattern3[i % len(pattern3)]:
-            scores[2] += 1
+        if answers[i] == a1[i%len(a1)]:
+            score[0] +=1
+        if answers[i] == a2[i%len(a2)]:
+            score[1]+=1
+        if answers[i] == a3[i%len(a3)]:
+            score[2]+=1
+            
+    # 1~3번 학생 중 많이 맞춘 학생의 번호가 먼저 나오게 해서 answer에 번호 추가
+    max_score = max(score)
     
-    # 가장 높은 점수를 찾고, 그 점수와 같은 사람을 모두 찾아 반환
-    max_score = max(scores)
-    result = [i + 1 for i, score in enumerate(scores) if score == max_score]
-    
-    return result
+    answer= [i+1 for i,s in enumerate(score) if s == max_score]
+  
+    return answer
