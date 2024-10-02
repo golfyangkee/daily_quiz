87c5baf77a689b561935f68884d5c78ea4db82a4
elly <103293064+golfyangkee@users.noreply.github.com>
2024-10-02 06:39:32 -0400

    [level 1] Title: 과일 장수, Time: 203.26 ms, Memory: 21.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,4 +1,15 @@
 def solution(k, m, score):
+    score.sort(reverse=True)
+    answer = 0
+    
+    for i in range(0,len(score), m):
+        if len(score[i:i+m])==m: # 남은 과일의 수가 m개인 경우에만 박스 만듦
+            answer+=min(score[i:i+m])*m
+            # answer+=score[i]하면 최대 점수가 아니라 최소 점수로 만들어지니까
+    return answer
+
+'''
+def solution(k, m, score):
     score.sort(reverse=True)  # 점수를 내림차순으로 정렬 (높은 점수부터 상자를 채우기 위해)
     
     total_score = 0
@@ -9,3 +20,4 @@ def solution(k, m, score):
             total_score += min(score[i:i+m]) * m  # 각 그룹의 최소 점수 * m
     
     return total_score
+'''
