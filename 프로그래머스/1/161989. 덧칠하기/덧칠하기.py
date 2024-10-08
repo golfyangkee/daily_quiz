c4f6191a7182d936c35bcfa763e197ad18a32b71
elly <103293064+golfyangkee@users.noreply.github.com>
2024-10-08 19:13:33 +0900

    [level 1] Title: 덧칠하기, Time: 5.97 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,15 +1,8 @@
 def solution(n, m, section):
-    # 페인트칠 횟수
-    count = 0
-    # 마지막으로 페인트칠한 구역의 끝 번호
-    last_painted = 0
-    
-    for s in section:
-        # 현재 구역이 마지막으로 칠한 구역 범위 밖에 있다면
-        if s > last_painted:
-            # 새로운 롤러로 칠함
-            count += 1
-            # 이번 롤러로 칠한 구역의 끝 범위를 갱신 (s부터 s + m - 1까지 칠함)
-            last_painted = s + m - 1
-            
-    return count
+    answer = 1
+    pre = section[0]
+    for i in section:
+        if pre+m-1<i:
+            answer+=1
+            pre=i # 덧칠한 경우 pre의 위치를 매번 변경해줘야 한다
+    return answer
