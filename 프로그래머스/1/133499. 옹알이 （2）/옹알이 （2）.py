42d1517af290aa85a1e24b3035b36e23ea2f7632
elly <103293064+golfyangkee@users.noreply.github.com>
2024-10-12 13:45:26 +0900

    [level 1] Title: 옹알이 (2), Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -3,8 +3,11 @@ def solution(babbling):
     result = ["aya", "ye", "woo", "ma"]
     for i in babbling:
         for j in result:
-            if j*2 not in i:
-                i=i.replace(j,' ')
-        if i.strip()=='':
+            if j*2 not in i: # 연속으로 두번 반복되지 않아야 한다.
+                i=i.replace(j,' ') 
+                # ' '로 변경하는 이유는 "ayayeayayeaya" 인 경우 
+                # aya 지우면 yeye만 남게 되니까 
+                # 공백을 줘서 ' ye ye ' 이런식으로 만들어야 하니까
+        if i.strip()=='': # strip은 공백을 제거한다.
             answer+=1
     return answer
