ca1451d686c79f255e63212f77f0080def4cac61
elly <103293064+golfyangkee@users.noreply.github.com>
2024-10-23 12:47:59 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.79 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,5 +1,25 @@
 def solution(keymap, targets):
     answer = []
+    hs = {} # 각 요소에 가장 최단 수의 알파벳들을 넣을 튜플
+    for k in keymap:
+        for i, ch in enumerate(k):
+            hs[ch] = min(i + 1, hs[ch]) if ch in hs else i + 1
+            # 만약에 hs 안에 같은 문자가 있다면 더 작은 값을 넣는다.
+        
+    for i, t in enumerate(targets):
+        ret = 0
+        for ch in t:
+            if ch not in hs: # 없으면 break
+                ret = - 1
+                break
+            ret += hs[ch] # hs 안에 있다면 valuse 값 더하기
+        answer.append(ret)
+
+    return answer
+
+'''
+def solution(keymap, targets):
+    answer = []
     
     for target in targets:
         sum_, fail = 0, False
@@ -14,4 +34,5 @@ def solution(keymap, targets):
         if fail : answer.append(-1)
         else : answer.append(sum_)
     
-    return answer
+    return answer 
+'''
