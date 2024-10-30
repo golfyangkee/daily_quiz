d9639c72b3b7d9f070042d02e4eb9519d0e60961
elly <103293064+golfyangkee@users.noreply.github.com>
2024-10-30 13:52:54 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,3 +1,20 @@
+'''
+# 이해해보기
+def solution(n, lost, reserve):
+    _reserve = [r for r in reserve if r not in lost] # 체육복 개수 2개인 학생
+    _lost = [l for l in lost if l not in reserve] # 체육복 개수 0개인 학생
+    for r in _reserve: # 즉 2개 여유분 있는 친구들
+        # 앞 뒤 숫자 구하기
+        f = r - 1
+        b = r + 1
+        # 잃어버렸더라도 앞 뒤가 친구가 여유가 있으니까 잃어버린게 아니다.
+        if f in _lost: # 로스트에 f가 있으면
+            _lost.remove(f) # 로스트에서 f 없애기
+        elif b in _lost: # 로스트에 b가 있으면 
+            _lost.remove(b) # 로스트에서 b 없애기
+    return n - len(_lost) # 진짜로 앞 뒤로도 구할 수 없는 친구들의 수를 뺀다.
+'''
+# 내가 처음으로 푼 것
 def solution(n, lost, reserve):
     answer = 0
     r = {}
