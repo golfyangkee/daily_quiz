c8825157193c4ecf4c453f615b4bfef3603bd519
elly <103293064+golfyangkee@users.noreply.github.com>
2024-10-31 16:07:42 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,6 +1,11 @@
 def solution(s):
+    return s.isdigit() and len(s) in [4,6]
+'''
+내가 푼 것
+def solution(s):
     r = ''.join(i for i in s if i.isdigit())
     if len(s) == 4 or len(s)==6:
         if len(r)==len(s):
             return True
     return False
+'''
