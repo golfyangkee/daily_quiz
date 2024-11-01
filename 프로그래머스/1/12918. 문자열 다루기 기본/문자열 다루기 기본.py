83575157774e2304a57dfe5e81012c6e24244d16
elly <103293064+golfyangkee@users.noreply.github.com>
2024-11-01 14:24:15 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,5 +1,7 @@
 def solution(s):
-    return s.isdigit() and len(s) in [4,6]
+    return s.isdigit() and len(s) in [4,6] 
+    # s 가 숫자로만 되어 있다면 True, s의 길이가 4 또는 6이면 True 반환해서
+    # 둘다 True이면 True, 하나라도 False면 False 리턴
 '''
 내가 푼 것
 def solution(s):
