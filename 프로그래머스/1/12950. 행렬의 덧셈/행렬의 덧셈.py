ada3bd0fc0543a34c1d53c8d08dc9c305ffc8bef
elly <103293064+golfyangkee@users.noreply.github.com>
2024-11-02 21:32:54 +0900

    [level 1] Title: 행렬의 덧셈, Time: 34.39 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,4 +1,17 @@
 def solution(arr1, arr2):
+    n = len(arr1)
+    answer=[]
+    for i in range(n):
+        row = [] # 한 행렬 크기 저장할 리스트
+        
+        for j in range(len(arr1[0])):
+        # for j in range(n): 하면 안된다.
+        # 정사각형 배열이 아니라 그냥 arr1과 arr2의 행렬 크기가 같다는 의미이다.
+            row.append(arr1[i][j]+arr2[i][j])
+        answer.append(row)
+    return answer
+'''
+def solution(arr1, arr2):
     answer = []
     for i in range(len(arr1)):
         row = []
@@ -6,3 +19,4 @@ def solution(arr1, arr2):
             row.append(arr1[i][j]+arr2[i][j])
         answer.append(row)
     return answer
+'''
