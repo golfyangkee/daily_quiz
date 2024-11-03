d125da724d34136a9e1d81b0312ac393f1c307bf
elly <103293064+golfyangkee@users.noreply.github.com>
2024-11-03 22:26:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 180.54 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,11 +1,18 @@
 def solution(arr):
+    # answer = []
+    # answer.append(arr[0]) # 처음꺼는 무조건 들어간다.
+    # for i in range(1, len(arr)):
+    #     if arr[i] != arr[i-1]:
+    #         answer.append(arr[i])
+    # return answer
+
+   # 다른 풀이
     answer = []
-    answer.append(arr[0]) # 처음꺼는 무조건 들어간다.
-    for i in range(1, len(arr)):
-        if arr[i] != arr[i-1]:
-            answer.append(arr[i])
+    for i in arr:
+        if answer[-1:] == [i]: continue 
+        # a의 마지막 값인데 a[-1:]하면 a에 아무것도 없어도 실행된다.
+        answer.append(i)
     return answer
-    
     
     
     '''
