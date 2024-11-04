af205759454c47bc289995e718273e1a856d43bc
elly <103293064+golfyangkee@users.noreply.github.com>
2024-11-04 19:04:37 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,4 +1,20 @@
 def solution(n, m):
+    result=[] # 겹치는 숫자 넣을 리스트
+    answer = [] # return 할 리스트
+    
+    for i in range(1,max(n,m)+1):
+        if n%i==0 and m%i==0:
+            result.append(i)
+            
+    # 최대공약수 집어 넣기
+    answer.append(max(result)) 
+    
+    # 최소공배수는 두 수의 곱을 최대공약수로 나눈 값이다.
+    answer.append(n*m/max(result)) 
+    return answer
+    
+    
+    '''
     answer = []
     if n>m:
         n, m = m , n
@@ -10,3 +26,4 @@ def solution(n, m):
     answer.append(max_a)
     answer.append(n*m/max_a)
     return answer
+    '''
