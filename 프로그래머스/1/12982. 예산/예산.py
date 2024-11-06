eeba3aee94637336a5af69bc73b224236055f8eb
elly <103293064+golfyangkee@users.noreply.github.com>
2024-11-07 06:46:41 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,4 +1,16 @@
 def solution(d, budget):
+    #  오름차순으로 나열 한 후 
+    # 남은 금액이 현재 금액보다 크면 주는 형식을 통해 최대한 많은 부서에 지원해 줄 수 있다.
+    answer=0
+    d.sort()
+    for i in d:
+        if i<=budget:
+            answer+=1
+            budget-=i
+    return answer
+
+'''
+def solution(d, budget):
     answer = 0
     d.sort()
     for i in d:
@@ -6,3 +18,4 @@ def solution(d, budget):
             answer+=1
             budget-=i
     return answer
+'''
