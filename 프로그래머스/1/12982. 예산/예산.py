5fe44c89f60ee550962429540f189e4704942ade
elly <103293064+golfyangkee@users.noreply.github.com>
2024-11-08 06:37:02 +0900

    [level 1] Title: 예산, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,4 +1,9 @@
 def solution(d, budget):
+    d.sort()
+    while budget < sum(d): # 합계가 budget보다 크면 뒤에서부터 하나씩 제거 후 개수 리턴
+        d.pop()
+    return len(d)
+    '''
     #  오름차순으로 나열 한 후 
     # 남은 금액이 현재 금액보다 크면 주는 형식을 통해 최대한 많은 부서에 지원해 줄 수 있다.
     answer=0
@@ -8,6 +13,7 @@ def solution(d, budget):
             answer+=1
             budget-=i
     return answer
+    '''
 
 '''
 def solution(d, budget):
