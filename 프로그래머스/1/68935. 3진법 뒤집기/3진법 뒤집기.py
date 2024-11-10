7671d1aa6c002e40af75942e4c9b2805fac23858
elly <103293064+golfyangkee@users.noreply.github.com>
2024-11-10 12:01:22 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,8 +1,20 @@
 def solution(n):
     answer = ''
     
+    while n>0:
+        answer += str(n%3) # str에 더해버리면 자연스럽게 뒤집어짐
+        n //=3 # 몫만 빼서 n값 변경
+    
+    return int(answer, 3) # int는 10진수로 표현하는 것인데 매개변수에 진수 넣으면 됨
+    
+
+'''
+def solution(n):
+    answer = ''
+    
     while n > 0:
         answer += str(n%3)
         n//=3
     
     return int(answer, 3)
+'''
