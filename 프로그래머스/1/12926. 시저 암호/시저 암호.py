29ba27720bf02494b34636d783807771b3ca3d7a
elly <103293064+golfyangkee@users.noreply.github.com>
2024-11-14 23:18:57 +0900

    [level 1] Title: 시저 암호, Time: 1.89 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,38 @@
+def solution(s, n):
+    answer = ''
+    for i in s:
+        result = ord(i)+n
+        if i ==' ':
+            answer+=i            
+        elif i.isupper() :
+            if result > 90:
+                answer += chr(64+result%90)
+                # 왜 65가 아니라 64이냐면 91은 나머지가 1인데 65가 되어야 하니까
+            else:
+                answer += chr(result)
+        else:
+            if result > 122:
+                answer  += chr(96 + result%122)
+            else:
+                answer += chr(result)
+    return answer
+# def solution(s, n):
+#     answer = ''
+#     '''65-90= 26개
+#     97-122'''
+#     for i in s:
+#         if i == ' ':
+#             answer+=i
+#         elif 'A'<=i<='Z':
+#             result=ord(i)+n%27
+#             if result>90:
+#                 answer+=chr(65+(result%90-1))
+#             else:
+#                 answer+=chr(result)
+#         elif 'a'<=i<='z':
+#             result=ord(i)+n%27
+#             if result>122:
+#                 answer+=chr(97+(result%122-1))
+#             else:
+#                 answer+=chr(result)
+#     return answer
