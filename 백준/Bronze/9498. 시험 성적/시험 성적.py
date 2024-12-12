f5d61d6307e4fe23b167c01997b94a27a3b88f7b
elly <103293064+golfyangkee@users.noreply.github.com>
2024-12-12 11:41:47 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,7 @@
+n = int(input())
+# 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F
+if n>=90 : print('A')
+elif n>=80 : print('B')
+elif n>=70: print('C')
+elif n>=60: print('D')
+else: print('F')
