61907cf9fa437e0ee726b039870aa5a8aee3168f
elly <103293064+golfyangkee@users.noreply.github.com>
2024-12-12 11:39:47 +0900

    Update 두 수 비교하기.py

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -1,7 +1,17 @@
 A, B = map(int, input().split())
+'''
 if A>B:
     print('>')
 elif A == B:
     print('==')
 else:
     print('<')
+'''
+print("==" if A==B else "><"[A<B])
+'''
+"><"[A<B]의 의미
+문자열 "><"는 두 문자 >와 <로 이루어진 문자열입니다.
+[A<B]는 조건 A < B의 결과(True 또는 False)를 숫자 0 또는 1로 변환합니다:
+A < B가 True이면 1.
+A < B가 False이면 0.
+'''
