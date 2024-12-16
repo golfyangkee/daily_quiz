1546ae9d265cdccf836e7fb0b50b095b4037d7ba
elly <103293064+golfyangkee@users.noreply.github.com>
2024-12-16 13:44:05 +0900

    Update A＋B － 5.py

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -5,3 +5,9 @@ a, b = map(int, input().split())
 while a != 0 and b !=0:
     print(a+b)
     a, b = map(int, input().split())
+'''
+for line in sys.stdin:
+    a, b = map(int, line.split())
+    if a + b != 0:
+        print(a + b)
+'''
