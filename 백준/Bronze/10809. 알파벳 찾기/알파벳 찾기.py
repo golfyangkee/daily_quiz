75d2314df9bd7191cf29d5316964ff975c817413
elly <103293064+golfyangkee@users.noreply.github.com>
2024-12-20 13:56:37 +0900

    Update 알파벳 찾기.py

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -9,3 +9,14 @@ for i in my_str:
     my_dict[i]=my_str.index(i)
 
 print(*my_dict.values())
+
+'''
+find 함수
+반환값
+**find()**는 주어진 substring이 string에서 처음으로 나타나는 인덱스(정수)를 반환합니다.
+만약 substring이 string에 없다면, **-1**을 반환합니다.
+
+my_str = input()
+for i in range(97, 123):
+    print(my_str.find(chr(i)), end=' ')
+'''
