66bda609178c6d22d184cbd8a109f3ca5b2d46df
elly <103293064+golfyangkee@users.noreply.github.com>
2024-12-20 13:34:16 +0900

    [Bronze I] Title: 평균, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,13 +1,7 @@
-n = int(input())  # 데이터의 개수
-my_list = list(map(int, input().split()))  # 리스트에 입력된 값들
-
-# 최대값 구하기
+n = int(input())
+my_list = list(map(int, input().split()))
 num_max = max(my_list)
-
-# 각 값들을 최대값에 비례하여 100을 기준으로 변환
 for i in range(n):
-    my_list[i] = my_list[i] / num_max * 100
-
-# 평균 구하기
-avg_my_list = sum(my_list) / n
+    my_list[i] = my_list[i]/num_max *100
+avg_my_list = sum(my_list)/n
 print(avg_my_list)
