1c6acc5277aa3d9abf619c40c3672a9b1f145b6e
elly <103293064+golfyangkee@users.noreply.github.com>
2025-01-01 22:05:35 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,21 @@
+n = int(input())  # 단어 개수 입력
+result = 0  # 그룹 단어 개수
+
+for _ in range(n):
+    s = input()  # 단어 입력
+    seen = set()  # 등장한 문자 저장
+    prev = None  # 이전 문자
+    
+    is_group_word = True
+    for char in s:
+        if char != prev:  # 새로운 문자 등장
+            if char in seen:  # 이미 나온 문자라면 그룹 단어가 아님
+                is_group_word = False
+                break
+            seen.add(char)  # 새로운 문자 저장
+        prev = char  # 이전 문자 갱신
+    
+    if is_group_word:
+        result += 1
+
+print(result)
