3106f092db5f8467d8613789869ea68b0df7d7b2
elly <103293064+golfyangkee@users.noreply.github.com>
2025-01-01 22:10:19 +0900

    Update 그룹 단어 체커.py

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -19,3 +19,15 @@ for _ in range(n):
         result += 1
 
 print(result)
+
+'''
+N = int(input())
+count = 0
+
+for _ in range(N):
+  string = input()
+  if list(string) == sorted(string, key=string.find):
+    count += 1
+
+print(count)
+'''
